5ddcb56061e01b730d2cd04c69559b7ca67db8be
anodynos <agelos.pikoulas@gmail.com>
2014-10-08 17:31:52 +0300

    browserify version '3.46.1'

--- punycode.js
+++ punycode.js
@@ -3,10 +3,11 @@
 From [node2web](http://github.com/anodynos/node2web) collection,
 should/will be exposed as 'punycode' to [bower](http://bower.io) for *browser* usage.
 
-browserify version: '3.24.10', build date 'Tue Sep 16 2014 02:23:13 GMT+0300 (EEST)' 
+browserify version: '3.46.1', build date 'Wed Oct 08 2014 17:31:52 GMT+0300 (EEST)' 
 **/
 !function(e){if("object"==typeof exports)module.exports=e();else if("function"==typeof define&&define.amd)define(e);else{var f;"undefined"!=typeof window?f=window:"undefined"!=typeof global?f=global:"undefined"!=typeof self&&(f=self),f.punycode=e()}}(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(_dereq_,module,exports){
-(function (global){/*! http://mths.be/punycode v1.2.3 by @mathias */
+(function (global){
+/*! http://mths.be/punycode v1.2.4 by @mathias */
 ;(function(root) {
 
 	/** Detect free variables */
@@ -236,7 +237,6 @@ browserify version: '3.24.10', build date 'Tue Sep 16 2014 02:23:13 GMT+0300 (EE
 		    k,
 		    digit,
 		    t,
-		    length,
 		    /** Cached calculation results */
 		    baseMinusT;
 
@@ -472,7 +472,7 @@ browserify version: '3.24.10', build date 'Tue Sep 16 2014 02:23:13 GMT+0300 (EE
 		 * @memberOf punycode
 		 * @type String
 		 */
-		'version': '1.2.3',
+		'version': '1.2.4',
 		/**
 		 * An object of methods to convert from JavaScript's internal character
 		 * representation (UCS-2) to Unicode code points, and back.
@@ -498,10 +498,10 @@ browserify version: '3.24.10', build date 'Tue Sep 16 2014 02:23:13 GMT+0300 (EE
 		typeof define.amd == 'object' &&
 		define.amd
 	) {
-		define(function() {
+		define('punycode', function() {
 			return punycode;
 		});
-	}	else if (freeExports && !freeExports.nodeType) {
+	} else if (freeExports && !freeExports.nodeType) {
 		if (freeModule) { // in Node.js or RingoJS v0.8.0+
 			freeModule.exports = punycode;
 		} else { // in Narwhal or RingoJS v0.7.0-
@@ -514,6 +514,7 @@ browserify version: '3.24.10', build date 'Tue Sep 16 2014 02:23:13 GMT+0300 (EE
 	}
 
 }(this));
+
 }).call(this,typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
 },{}],2:[function(_dereq_,module,exports){
 module.exports = _dereq_('punycode');
